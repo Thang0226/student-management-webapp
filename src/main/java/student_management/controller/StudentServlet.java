9d4d82c2dc956a511ee4c155a7c284f418d5fc19
Thang0226 <thang.nd0226@gmail.com>
2024-11-29 14:26:08 +0700

    added feature: update (edit) student information

--- src/main/java/student_management/controller/StudentServlet.java
+++ src/main/java/student_management/controller/StudentServlet.java
@@ -29,7 +29,7 @@ public class StudentServlet extends HttpServlet {
 				showCreateForm(request, response);
 				break;
 			case "edit":
-//				showEditForm(request, response);
+				showEditForm(request, response);
 				break;
 			case "delete":
 //				showDeleteForm(request, response);
@@ -45,6 +45,23 @@ public class StudentServlet extends HttpServlet {
 
 	private void showCreateForm(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("student/create.jsp");
+		try {
+			dispatcher.forward(request, response);
+		} catch (ServletException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Student student = studentService.findById(id);
+		RequestDispatcher dispatcher;
+		if (student == null) {
+			dispatcher = request.getRequestDispatcher("error_404.jsp");
+		} else {
+			request.setAttribute("student", student);
+			dispatcher = request.getRequestDispatcher("student/edit.jsp");
+		}
 		try {
 			dispatcher.forward(request, response);
 		} catch (ServletException e) {
@@ -77,7 +94,7 @@ public class StudentServlet extends HttpServlet {
 				createStudent(request, response);
 				break;
 			case "edit":
-//				updateStudent(request, response);
+				updateStudent(request, response);
 				break;
 			case "delete":
 //				deleteStudent(request, response);
@@ -100,6 +117,28 @@ public class StudentServlet extends HttpServlet {
 		studentService.add(student);
 		request.setAttribute("message", "New student was created");
 		RequestDispatcher dispatcher = request.getRequestDispatcher("student/create.jsp");
+		try {
+			dispatcher.forward(request, response);
+		} catch (ServletException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String id_str = request.getParameter("id");
+		int id = Integer.parseInt(id_str);
+		String name = request.getParameter("name");
+		String score_str = request.getParameter("score");
+		int score = Integer.parseInt(score_str);
+
+		Student student = studentService.findById(id);
+		student.setName(name);
+		student.setScore(score);
+		studentService.update(id, student);
+
+		request.setAttribute("message", "Student information was modified");
+		request.setAttribute("student", student);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("student/edit.jsp");
 		try {
 			dispatcher.forward(request, response);
 		} catch (ServletException e) {
