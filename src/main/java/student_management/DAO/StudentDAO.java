85b334d4f3e93fb833706b6c8e4aa99d1341e6dd
Thang0226 <thang.nd0226@gmail.com>
2024-12-04 11:45:52 +0700

    create connection to database & create CRUD sql procedures

--- src/main/java/student_management/DAO/StudentDAO.java
+++ src/main/java/student_management/DAO/StudentDAO.java
@@ -3,9 +3,23 @@ package student_management.DAO;
 import student_management.model.Student;
 import student_management.service.StudentService;
 
+import java.sql.*;
 import java.util.List;
 
 public class StudentDAO implements StudentService {
+	private String jdbcURL = "jdbc:mysql://localhost:3306/student_management";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "123456";
+
+	protected Connection getConnection() throws SQLException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		return DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+	}
+
 	@Override
 	public List<Student> findAll() {
 		return null;
--- src/main/sql/script.sql
+++ src/main/sql/script.sql
@@ -25,3 +25,73 @@ insert into student (name, score, class_id) values
 	('Dung', 96, 2),
 	('Tri', 96, 3),
 	('Tung', 96, 1);
+    
+    
+    
+delimiter $
+create procedure list_students()
+begin
+	select * from student;
+end $
+delimiter ;
+
+delimiter $
+create procedure add_student(
+    in in_name varchar(20),
+    in in_score int,
+    in in_class_id int
+)
+begin
+	insert into student (name, score, class_id) values
+		(in_name, in_score, in_class_id);
+end $
+delimiter ;
+
+delimiter $
+create procedure update_student(
+	in in_id int,
+	in in_name varchar(20),
+    in in_score int,
+    in in_class_id int
+)
+begin
+	update student
+    set 
+		name = in_name,
+        score = in_score,
+        class_id = in_class_id
+	where id = in_id;
+end $
+delimiter ;
+
+delimiter $
+create procedure delete_student (in in_id int)
+begin
+	delete from student
+    where id = in_id;
+end $
+delimiter ;
+
+delimiter $
+create procedure find_student (in in_id int)
+begin
+	select name, score, class_id 
+    from student
+    where id = in_id;
+end $
+delimiter ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
